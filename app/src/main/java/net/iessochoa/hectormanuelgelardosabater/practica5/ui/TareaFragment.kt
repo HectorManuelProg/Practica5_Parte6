b4364e2107fd00dcbb86000a8d4c1fe9d7caaeab
Hector <hectormanuel.gs@hotmail.com>
2023-11-13 22:58:18 +0100

    Creado el metodo iniciaspPrioridad

--- app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica5/ui/TareaFragment.kt
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica5/ui/TareaFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
 import net.iessochoa.hectormanuelgelardosabater.practica5.R
 import net.iessochoa.hectormanuelgelardosabater.practica5.databinding.FragmentTareaBinding
@@ -19,6 +20,7 @@ class TareaFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +38,38 @@ class TareaFragment : Fragment() {
       /*  binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_TareaFragment_to_FirstFragment)
         }*/
+
+        iniciaSpCategoria()
+        iniciaSpPrioridad()
+    }
+
+    private fun iniciaSpCategoria() {
+        ArrayAdapter.createFromResource(
+            //contexto suele ser la Activity
+            requireContext(),
+            //array de strings
+            R.array.categoria,
+            //layout para mostrar el elemento seleccionado
+            android.R.layout.simple_spinner_item).also { adapter ->
+            // Layout para mostrar la apariencia de la lista
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // asignamos el adaptador al spinner
+            binding.spCategoria.adapter = adapter
+        }
+    }
+    private fun iniciaSpPrioridad() {
+        ArrayAdapter.createFromResource(
+            //contexto suele ser la Activity
+            requireContext(),
+            //array de strings
+            R.array.prioridad,
+            //layout para mostrar el elemento seleccionado
+            android.R.layout.simple_spinner_item).also { adapter ->
+            // Layout para mostrar la apariencia de la lista
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // asignamos el adaptador al spinner
+            binding.spPrioridad.adapter = adapter
+        }
     }
 
     override fun onDestroyView() {
