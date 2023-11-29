94738c11b804964ab1f47de925a0ea22d77f2a79
Hector <hectormanuel.gs@hotmail.com>
2023-11-29 13:12:04 +0100

    Parte 2

---
+++ app/src/main/java/model/Tarea.kt
@@ -0,0 +1,15 @@
+package model
+
+data class Tarea(
+var id:Long?=null,//id único
+val categoria:Int,
+val prioridad:Int,
+val pagado:Boolean,
+val estado:Int,
+val horasTrabajo:Int,
+val valoracionCliente:Float,
+val tecnico:String,
+val descripcion:String
+){
+
+}
