d45f5c72d39d73b0d8358bd9ea38ed2dcd057b76
Hector <hectormanuel.gs@hotmail.com>
2024-01-16 22:53:13 +0100

    fallo con el getDatabase en el repositorio

---
+++ app/src/main/java/model/db/TareasDataBase.kt
@@ -0,0 +1,35 @@
+package model.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import model.Tarea
+
+class TareasDataBase {
+    @Database(entities = arrayOf(Tarea::class), version = 1, exportSchema = false)
+    public abstract class TareasDataBase : RoomDatabase() {
+        abstract fun tareasDao(): TareasDao
+        companion object {
+// Singleton prevents multiple instances of database opening at the
+            // same time.
+            @Volatile
+            private var INSTANCE: TareasDataBase? = null
+            fun getDatabase(context: Context): TareasDataBase {
+// if the INSTANCE is not null, then return it,
+// if it is, then create the database
+                return INSTANCE ?: synchronized(this) {
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        TareasDataBase::class.java,
+                        "tareas_database"
+                    )
+                        .build()
+                    INSTANCE = instance
+// return instance
+                    instance
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/repository/Repository.kt
+++ app/src/main/java/repository/Repository.kt
@@ -2,15 +2,16 @@ package repository
 
 import android.app.Application
 import android.content.Context
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import model.Tarea
+import model.db.TareasDao
+import model.db.TareasDataBase
 import model.temp.ModelTempTareas
 
 object Repository {
     //instancia al modelo
-    private lateinit var modelTareas: ModelTempTareas
-    private val listaTareas = mutableListOf<Tarea>()
+    //private lateinit var modelTareas: ModelTempTareas
+    private lateinit var modelTareas: TareasDao
+    //private val listaTareas = mutableListOf<Tarea>()
     //el context suele ser necesario para recuperar datos
     private lateinit var application: Application
 
@@ -18,15 +19,16 @@ object Repository {
     operator fun invoke(context: Context) {
         this.application = context.applicationContext as Application
         //iniciamos el modelo
-        ModelTempTareas(application)
-        modelTareas = ModelTempTareas
+        //ModelTempTareas(application)
+       // modelTareas = ModelTempTareas
+        // iniciamos BD
+       modelTareas = TareasDataBase.getDatabase(application).tareasDao()
     }
 
     fun getTareasFiltroSinPagar (soloSinPagar:Boolean) = modelTareas.getTareasFiltroSinPagar(soloSinPagar)
-    fun addTarea(tarea: Tarea) = modelTareas.addTarea(tarea)
+    suspend fun addTarea(tarea: Tarea) = modelTareas.addTarea(tarea)
     suspend fun delTarea(tarea: Tarea) = modelTareas.delTarea(tarea)
     fun getTareasFiltroEstado(estado: Int) = modelTareas.getTareasFiltroEstado(estado)
-    fun getTareasFiltroSinPagarEstado(soloSinPagar:Boolean, estado:Int)=
-        modelTareas.getTareasFiltroSinPagarEstado(soloSinPagar,estado)
+    fun getTareasFiltroSinPagarEstado(soloSinPagar:Boolean, estado:Int)= modelTareas.getTareasFiltroSinPagarEstado(soloSinPagar,estado)
     fun getAllTareas() = modelTareas.getAllTareas()
 }
