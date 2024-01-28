3268d559579a0da30d825f5269feabb4585c66c2
Hector <hectormanuel.gs@hotmail.com>
2024-01-28 22:17:15 +0100

    Fin parte 7

--- app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica5/SettingsFragment.kt
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica5/SettingsFragment.kt
@@ -18,11 +18,22 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val buildVersion: Preference? = findPreference("buildVersion")
         //definimos la accion para la preferencia
         buildVersion?.setOnPreferenceClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://portal.edu.gva.es/03013224/"))
-            )
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://portal.edu.gva.es/03013224/")))
          //hay que devolver booleano para indicar si se acepta el cambio o no
             false
         }
-    }
 
+        val telefonoContactoPreference: Preference? = findPreference("telefonoContacto")
+        telefonoContactoPreference?.setOnPreferenceClickListener {
+            // Número de teléfono al que quieres llamar
+            val phoneNumber = "tel:966912260"
+            // Creamos un Intent con la acción ACTION_DIAL y la URI del número de teléfono
+            val dialIntent = Intent(Intent.ACTION_DIAL, Uri.parse(phoneNumber))
+            // Iniciamos la actividad del teléfono
+            startActivity(dialIntent)
+            // Devolvemos true para indicar que se acepta el cambio
+            true
+        }
+    }
 }
+
