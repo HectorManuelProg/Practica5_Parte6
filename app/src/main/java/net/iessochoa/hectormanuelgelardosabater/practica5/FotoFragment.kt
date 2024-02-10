6d828fec5bc60879214be899a572165a0900e345
Hector <hectormanuel.gs@hotmail.com>
2024-02-10 18:08:25 +0100

    Foto

--- app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica5/FotoFragment.kt
+++ app/src/main/java/net/iessochoa/hectormanuelgelardosabater/practica5/FotoFragment.kt
@@ -17,11 +17,20 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.core.content.ContentProviderCompat.requireContext
+import java.util.Locale
 
 class FotoFragment : Fragment() {
     private var _binding: FragmentFotoBinding? = null
+    private var uriFoto: Uri?=null
     private val binding get() = _binding!!
     //Array con los permisos necesarios
     private val PERMISOS_REQUERIDOS =
@@ -33,8 +42,12 @@ class FotoFragment : Fragment() {
             }
         }.toTypedArray()
 
+    private var imageCapture: ImageCapture? = null
+
+
     companion object {
         private const val TAG = "Practica5_CameraX"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,14 +63,68 @@ class FotoFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
-    {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Configurar el OnClickListener para el botón redondo
+        binding.btCapturaFoto.setOnClickListener {
+            // Llamar al método para capturar la foto
+            takePhoto()
+        }
+
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             solicitudPermisosLauncher.launch(PERMISOS_REQUERIDOS)
         }
+
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    private fun takePhoto() {
+// Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+// Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+// val name = "practica5_1"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+//funiona en versiones superiores a Android 9
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" +
+                         getString(R.string.app_name))
+            }
+        }
+// Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(
+                requireActivity().contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            .build()
+// Set up image capture listener, which is triggered after photo has
+// been taken
+                imageCapture.takePicture(
+                    outputOptions,
+                    ContextCompat.getMainExecutor(requireContext()),
+                    object : ImageCapture.OnImageSavedCallback {
+                        override fun onError(exc: ImageCaptureException) {
+                            Log.e(TAG, "Photo capture failed: ${exc.message}",
+                                exc)
+                        }
+                        override fun onImageSaved(output:
+                                                  ImageCapture.OutputFileResults) {
+                            val msg = "Photo capture succeeded:${output.savedUri}"
+                            Toast.makeText(requireContext(), msg,
+                                Toast.LENGTH_SHORT).show()
+                            Log.d(TAG, msg)
+                            binding.ivMuestra.setImageURI(output.savedUri)
+                            uriFoto=output.savedUri
+                        }
+                    }
+                )
     }
 
     private fun allPermissionsGranted() = PERMISOS_REQUERIDOS.all {
@@ -65,33 +132,38 @@ class FotoFragment : Fragment() {
                 PackageManager.PERMISSION_GRANTED
     }
 
-    /*private fun startCamera() {
-        // Inicializa el proveedor de la cámara
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+    private fun startCamera() {
+        //Se usa para vincular el ciclo de vida de las cámaras al propietario del ciclo de vida.
+        val cameraProviderFuture =
+            ProcessCameraProvider.getInstance(requireContext())
+        //Agrega un elemento Runnable como primer argumento
         cameraProviderFuture.addListener({
-            // Obtiene el proveedor de la cámara
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-            // Configura la vista previa de la cámara
-            val preview = Preview.Builder().build().also {
-                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-            }
-            // Selecciona la cámara trasera como predeterminada
+            // Esto se usa para vincular el ciclo de vida de nuestra cámara al LifecycleOwner dentro del proceso de la aplicación
+            val cameraProvider: ProcessCameraProvider =
+                cameraProviderFuture.get()
+            //Inicializa nuestro objeto Preview,
+            // llama a su compilación, obtén un proveedor de plataforma desde el visor y,
+            // luego, configúralo en la vista previa.
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            imageCapture = ImageCapture.Builder().build()
+            // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             try {
-                // Desvincula todos los casos de uso antes de volver a vincularlos
+            // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
-                // Vincula los casos de uso a la cámara
-                cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview)
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
             } catch (exc: Exception) {
-                Log.e(TAG, "Error al vincular los casos de uso", exc)
+                Log.e(TAG, "Use case binding failed", exc)
             }
+            //segundo argumento
         }, ContextCompat.getMainExecutor(requireContext()))
-    }*/
-
-    private fun startCamera() {
-        Toast.makeText(requireContext(),
-            "Camara iniciada…",
-            Toast.LENGTH_SHORT).show()
     }
 
     //Permite lanzar la solicitud de permisos al sistema operativo y actuar según el usuario
